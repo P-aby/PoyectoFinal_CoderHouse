238e7403d5a2338d61fc8cc2d715222b1dffbd8d
ronaldsdc <ronalddiaz.dg@gmail.com>
2022-07-30 20:58:50 -0600

    v1.008
    corregido TDA Queue

--- Assets/Scripts/TDA_Queue.cs
+++ Assets/Scripts/TDA_Queue.cs
@@ -16,14 +16,7 @@ public class TDA_Queue : MonoBehaviour
             
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-      //  if (collision.collider.transform.tag == "Player")
-       // {
-      //      enemigos.Dequeue();
-     //   }
-    //    Debug.Log("enemigos");
-  //  }
+  
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.transform.tag == "Player")
@@ -35,18 +28,8 @@ public class TDA_Queue : MonoBehaviour
     void SpawnEnemy()
     {
 
-        Instantiate(enemigos.Dequeue());//, new Vector3(Random.Range(-100, 100), 0, Random.Range(-50, 100)), Quaternion.identity);
-        //foreach(GameObject enemigos in ene)
-        // {
-        //    Debug.Log("enemigospawn");
-        //     enemigos.Enqueue(Instantiate(enemigo2, new Vector3(Random.Range(-100, 100), 0, Random.Range(-50, 100)), Quaternion.identity));
-        //  }
-
-        // for (int i = 0; i < quantity; i++)
-        // {
-        //     Debug.Log(i);
-        //     enemigos.Enqueue(Instantiate(enemigo2, new Vector3(Random.Range(-100, 100), 0, Random.Range(-50, 100)), Quaternion.identity));
-        // }
+        Instantiate(enemigos.Dequeue(), new Vector3(Random.Range(-100, 100), 0, Random.Range(-50, 100)), Quaternion.identity);
+        
 
     }
 }
