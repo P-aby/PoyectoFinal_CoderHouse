e17270aa264e10749c78c3e1ccd56c02519b468f
ronaldsdc <ronalddiaz.dg@gmail.com>
2022-07-30 21:07:08 -0600

    v1.010
    Agregado TDA Lista cristales

--- Assets/Scripts/TDA_List.cs
+++ Assets/Scripts/TDA_List.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class TDA_List : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<string> cristales = new List<string>();
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == "CristalRojo")
+        {
+            Destroy(other.transform.gameObject);
+            cristales.Add("Cristal rojo");
+        }
+        if (other.transform.name == "CristalAmarillo")
+        {
+            Destroy(other.transform.gameObject);
+            cristales.Add("Cristal amarillo");
+        }
+        if (other.transform.name == "CristalAzul")
+        {
+            Destroy(other.transform.gameObject);
+            cristales.Add("Cristal azul");
+        }
     }
 }
